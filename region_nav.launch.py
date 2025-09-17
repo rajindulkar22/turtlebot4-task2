5aa128274dd7a680d48e14cb9aad4dfc897860e7
rajindulkar22 <rajindulkar7@gmail.com>
2025-09-17 11:18:04 +0200

    Add files via upload

---
+++ region_nav.launch.py
@@ -0,0 +1,47 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # Path to turtlebot4_navigation package
+    tb4_nav_dir = get_package_share_directory('turtlebot4_navigation')
+
+    # Paths to map and nav2 config
+    map_file = '/home/raj/maps/test1_map.yaml'
+    params_file = '/home/raj/ros2_ws/config/my_nav2.yaml'
+
+    # Path to RViz config
+    rviz_config = os.path.join(
+        tb4_nav_dir, 'rviz', 'nav2_default_view.rviz'
+    )
+
+    return LaunchDescription([
+        # Start Nav2 localization (map_server + amcl + controller_server)
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(tb4_nav_dir, 'launch', 'localization.launch.py')),
+            launch_arguments={
+                'map': map_file,
+                'params_file': params_file,
+                'rviz': 'false'  # we'll launch rviz separately
+            }.items()
+        ),
+
+        # Start RViz2
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', rviz_config],
+            output='screen'
+        ),
+
+        # Start region_reporter
+        Node(
+            package='region_reporter',
+            executable='region_reporter',
+            output='screen'
+        )
+    ])
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'region_reporter'
+package_name = 'demo_package'
 
 setup(
     name=package_name,
@@ -10,17 +12,18 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='raj',
-    maintainer_email='raj@todo.todo',
+    maintainer_email='todo@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-             'region_reporter = region_reporter.region_reporter:main',
         ],
     },
 )
